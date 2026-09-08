0b82d7910a4c43b1758181a1bf2cf5cd4b7fabd1
GitHub Action <action@github.com>
2026-09-08 09:52:59 +0000

    Week 22

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+
+    private String name;
+
+    private double price;
+    public Car(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
--- src/TableTennis.java
+++
@@ -1,8 +0,0 @@
-public class TableTennis {
-
-    static void main(String[] args) {
-        System.out.println("table tennis");
-
-
-    }
-}
